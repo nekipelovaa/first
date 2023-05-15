abba4f5a12413f1e370260b4ed0445b7f40e22c4
Aleksey N <blindfoldeah@gmail.com>
2023-05-16 00:21:39 +0300

    Change string

--- main.go
+++ main.go
@@ -4,8 +4,8 @@ import "fmt"
 
 type SettlementId string
 
-var cityId SettlementId = "228edf0c-0c0d-4db6-bf0e-d508e68270a3"
-var strCityId string = "228edf0c-0c0d-4db6-bf0e-d508e68270a3"
+var cityId SettlementId = "228edf0c"
+var strCityId string = "228edf0c"
 
 func main() {
 	fmt.Println(cityId == SettlementId(strCityId))
