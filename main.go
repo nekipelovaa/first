5082d02bbcfe0492c39a2f9467a73f48e807906c
Aleksey N <blindfoldeah@gmail.com>
2023-05-15 17:43:25 +0300

    my firs commit

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+type SettlementId string
+
+var cityId SettlementId = "228edf0c-0c0d-4db6-bf0e-d508e68270a3"
+var strCityId string = "228edf0c-0c0d-4db6-bf0e-d508e68270a3"
+
+func main() {
+	fmt.Println(cityId == SettlementId(strCityId))
+	fmt.Println("1 + 1 =", 1.01+1.01)
+}
