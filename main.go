a27ab8c080a2961d192d3e098a35609e51efe4df
Aleksey N <blindfoldeah@gmail.com>
2023-05-16 00:08:53 +0300

    Add test()

--- main.go
+++ main.go
@@ -10,4 +10,9 @@ var strCityId string = "228edf0c-0c0d-4db6-bf0e-d508e68270a3"
 func main() {
 	fmt.Println(cityId == SettlementId(strCityId))
 	fmt.Println("1 + 1 =", 1.01+1.01)
+	test()
+}
+
+func test() {
+	fmt.Printf("%d число", 3)
 }
