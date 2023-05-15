5ef643d1246988f98eb6df457ea82b5a70ccecb4
Aleksey N <blindfoldeah@gmail.com>
2023-05-15 23:06:59 +0300

    Test

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 type SettlementId string
-fgd
+
 var cityId SettlementId = "228edf0c-0c0d-4db6-bf0e-d508e68270a3"
 var strCityId string = "228edf0c-0c0d-4db6-bf0e-d508e68270a3"
 
